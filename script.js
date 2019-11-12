62a8502264d3012346f0bdd737c328b76f32d056
stevie2codes <stevie2codes@gmail.com>
2019-11-11 21:55:10 -0600

    styled modal

--- script.js
+++ script.js
@@ -155,5 +155,9 @@ leader.onclick = function(){
 span.onclick = function() {
     modal.style.display = "none";
 }
-
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
 
